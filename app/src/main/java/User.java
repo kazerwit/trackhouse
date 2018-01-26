b2643566b14f95952ba46c1d9032d0b50af3e0a0
kazerwit <numberalternative@gmail.com>
2018-01-26 15:36:47 -0800

    SignUp activity - add password conf. field and username field

---
+++ app/src/main/java/User.java
@@ -0,0 +1,19 @@
+/**
+ * Created by lordis on 1/26/2018.
+ */
+
+public class User {
+
+    public String username;
+    public String email;
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+}
